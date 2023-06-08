715b089a86c83caa1bf84325efd8164d77f20505
nattyjojo <nattyjojo9@gmail.com>
2023-06-08 16:44:45 +0200

    still working on create

--- 3.Create/index.js
+++ 3.Create/index.js
@@ -1,49 +1,45 @@
 
-
+// Modify the script.js to create a new <section> with a random background-color for each learner in your group. 
+// This section should contain a paragraph with the name of the learner. Those sections should be appended in the <article>
 const groupList = ["LeslieL", "Ignace", "Leila_1111", "itsafa", "Louis", "Lucie", "Noah", "lexandra",
  "Jean-Louis", "Antoine Stru", "Athi Lakshmi", "azizabacc", "camilaassunca", "Florentina", 
  "francesca", "Giorgi Patsia", "GlaucielleSa",
  "princekaf", "Raad", "Walid", "William Polet", "ZuzanaDroppova", "Alizée", "adam98", "Louis", "SophieB"
 ]
-let picColor = "0123456789QBCDEF";
+groupList.sort()
+let picColor = "0123456789ABCDEF";
 let storeColor = "#";
-function randomBg(){
-    let coun = "123456"
+const randomBg = () => {
+    let coun = "123456";
     for(let i of coun){
         storeColor += picColor[Math.floor(Math.random() * picColor.length)];
     }
-
 }
 
-function create(){  
+const create = () => {  
     for(let x = 0; x < groupList.length; x++){
-        let section = document.createElement("section");
         randomBg();
-        // how do call randomBg to work?
-        section.style.backgroundColor=storeColor;
+        let section = document.createElement("section");
         const paragraph = document.createElement("p");
         section.appendChild(paragraph);
         const getArticle = document.querySelector("article");
+        getArticle.setAttribute("onload", "changes");
         getArticle.appendChild(section);
-        
+        let Paragraphs = document.querySelectorAll("p");
+        let add = Paragraphs[x].innerText = groupList[x];
+        let bgcolor = section.style.backgroundColor= storeColor;
+        let G = storeColor.slice(3, 5);
+        if(G > 00){
+            section.style.color="white"
+        }else{
+            section.style.color="black";
+        }
+        console.log(storeColor);
+        storeColor =  "#";
     }
-    let Paragraphs = document.querySelectorAll("p");
-    for(let c = 0; c < Paragraphs.length; c++){
-        let add = Paragraphs[c].innerHTML=groupList[c];
-    
-
-    }
-    
-
 }
-
 create();
-
 /*
-    Modify the script.js to create a new <section> with a random background-color for each learner in your group. 
-    This section should contain a paragraph with the name of the learner. Those sections should be appended in the <article>
-
     If the background is dark the text should be white, if the background is light the text should be black
     Find a way so that everytime you load the page the order of the elements changes!
-
 */
